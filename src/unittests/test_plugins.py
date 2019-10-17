875ed7573ee9d63aa5d10271ca298308fd91b4e8
Daniel Troeder <troeder@univention.de>
2019-10-17 09:00:47 +0200

    Bug #50375: enable running plugin test outside container

--- src/unittests/test_plugins.py
+++ src/unittests/test_plugins.py
@@ -28,12 +28,14 @@
 # <http://www.gnu.org/licenses/>.
 
 import shutil
+from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
+import id_sync.constants
 import id_sync.plugin_loader
 import id_sync.plugins
-from id_sync.constants import PLUGIN_DIRS, PLUGIN_PACKAGE_DIRS
 
 
 class DummyPluginSpec:
@@ -44,16 +46,26 @@ class DummyPluginSpec:
 
 @pytest.fixture
 def example_plugin(random_name):
+    # replace /var/lib/univention-appcenter/apps/id-sync with path below /tmp
+    # and /id-sync/src with ../..
+    tmp_dir = Path("/tmp/", random_name())
+    default_package_dir = Path(__file__).parent.parent / "plugins/packages"
+    custom_package_base_dir = tmp_dir / "plugins/packages"
+    custom_package_base_dir.mkdir(parents=True)
+    mock_package_dirs = (default_package_dir, custom_package_base_dir)
+    default_plugin_dir = Path(__file__).parent.parent / "plugins/plugins"
+    custom_plugin_dir = tmp_dir / "plugins/plugins"
+    custom_plugin_dir.mkdir(parents=True)
+    mock_plugin_dirs = (default_plugin_dir, custom_plugin_dir)
+
     id_sync.plugins.plugin_manager.add_hookspecs(DummyPluginSpec)
-    custom_package_dir = [d for d in PLUGIN_PACKAGE_DIRS if "appcenter" in str(d)][0]
     custom_package_name = random_name(ints=False)
-    custom_package_dir = custom_package_dir / custom_package_name
-    custom_package_dir.mkdir()
+    custom_package_dir = custom_package_base_dir / custom_package_name
+    custom_package_dir.mkdir(parents=True)
     custom_module_name = random_name(ints=False)
     custom_module_path = custom_package_dir / f"{custom_module_name}.py"
     with open(custom_module_path, "w") as fp:
         fp.write(CUSTOM_TEST_MODULE_IN_PACKAGE)
-    custom_plugin_dir = [d for d in PLUGIN_DIRS if "appcenter" in str(d)][0]
     custom_plugin_name = random_name(ints=False)
     custom_plugin_path = custom_plugin_dir / f"{custom_plugin_name}.py"
     with open(custom_plugin_path, "w") as fp:
@@ -62,22 +74,16 @@ def example_plugin(random_name):
                 module_name=custom_module_name, package_name=custom_package_name
             )
         )
-    default_plugin_dir = [d for d in PLUGIN_DIRS if "appcenter" not in str(d)][0]
     default_plugin_name = random_name(ints=False)
     default_plugin_path = default_plugin_dir / f"{default_plugin_name}.py"
     with open(default_plugin_path, "w") as fp:
         fp.write(DEFAULT_DUMMY_PLUGIN)
-    yield
+
+    yield mock_package_dirs, mock_plugin_dirs
+
     id_sync.plugins.plugin_manager.unregister(DummyPluginSpec)
-    shutil.rmtree(custom_package_dir)
-    custom_plugin_path.unlink()
+    shutil.rmtree(tmp_dir)
     default_plugin_path.unlink()
-    for path in (custom_plugin_dir / "__pycache__").glob(f"{custom_plugin_name}.*.pyc"):
-        path.unlink()
-    try:
-        (custom_plugin_dir / "__pycache__").rmdir()
-    except OSError:
-        pass
     for path in (default_plugin_dir / "__pycache__").glob(
         f"{default_plugin_name}.*.pyc"
     ):
@@ -89,7 +95,11 @@ def example_plugin(random_name):
 
 
 def test_load_plugins(example_plugin, random_int):
-    id_sync.plugin_loader.load_plugins()
+    mock_package_dirs, mock_plugin_dirs = example_plugin
+    with patch.object(
+        id_sync.plugin_loader, "PLUGIN_PACKAGE_DIRS", mock_package_dirs
+    ), patch.object(id_sync.plugin_loader, "PLUGIN_DIRS", mock_plugin_dirs):
+        id_sync.plugin_loader.load_plugins()
     arg1 = random_int()
     arg2 = random_int()
     res = id_sync.plugins.plugin_manager.hook.dummy_func(arg1=arg1, arg2=arg2)
