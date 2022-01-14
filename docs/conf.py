a5cbb6711e62378893950e27412643b41eef40d5
Tobias Wenzel <wenzel@univention.de>
2022-01-14 09:23:04 +0100

    Bug #54331: change version in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, univention"
 author = "univention"
 
 # The full version, including alpha/beta/rc tags
-release = "2.1.1"
+release = "2.2.0"
 
 # -- General configuration ---------------------------------------------------
 
