1d8ffde1b5be23055d60ab76594c0a06cf4151b6
Nico Gulden <gulden@univention.de>
2023-08-17 12:34:59 +0200

    doc: Activate document search with Typesense
    Refs univention/ucsschool#1083

--- docs/conf.py
+++ docs/conf.py
@@ -106,6 +106,9 @@ doc_basename = "ucsschool-id-connector"
 html_theme_options = {
     "pdf_download_filename": f"{doc_basename}.pdf",
     "show_source_license": True,
+    "typesense_search": True,
+    "typesense_document": doc_basename,
+    "typesense_document_version": "latest",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
