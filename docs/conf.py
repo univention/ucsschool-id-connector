9cdb6984986e2d8c850e11b12f7ac87872aa6e3b
Jürn Brodersen <brodersen@univention.de>
2024-05-23 14:31:12 +0200

    Clean up version handling
    Issue univention/components/ucsschool-id-connector#59

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,9 @@ copyright = "2021, Univention GmbH"
 author = "Univention GmbH"
 
 # The full version, including alpha/beta/rc tags
-release = "2.3.4"
+with open("../VERSION.txt", "r") as fp:
+    version = fp.read().strip()
+release = version
 
 # -- General configuration ---------------------------------------------------
 
